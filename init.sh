8697a5bafce0a1f91ff228ebe79ed87b40c03cbb
%username% <%useremail%>
2019-06-24 12:51:47 +0300

    commit

--- init.sh
+++ init.sh
@@ -2,4 +2,5 @@ sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 cd /home/box/web && sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:application
+sudo /etc/init.d/gunicorn restart﻿﻿
 
