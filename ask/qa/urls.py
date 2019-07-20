9e952560379ba44d5efd1dc6e4233ce5b2d6480e
%username% <%useremail%>
2019-07-20 14:42:16 +0300

    commit

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -2,11 +2,11 @@ from django.conf.urls import url
 from qa.views import test
 
 urlpatterns = [
-	url(r'^$', 'test'),
-	url(r'^login/', 'test'),
-	url(r'^signup/', 'test'),
-	url(r'^question/(?P<index>[d+])', 'test'),
-	url(r'^ask/', 'test'),
-	url(r'^popular/', 'test'),
-	url(r'^new/', 'test'),
+	url(r'^$', 'test', name = 'test'),
+	url(r'^login/', 'test', name = 'test'),
+	url(r'^signup/', 'test', name = 'test'),
+	url(r'^question/(?P<index>[d+])', 'test', name = 'test'),
+	url(r'^ask/', 'test', name = 'test'),
+	url(r'^popular/', 'test', name = 'test'),
+	url(r'^new/', 'test', name = 'test'),
 ]
