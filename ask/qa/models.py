b48f9640954c79c07b1d15348a7124abba1077ad
%username% <%useremail%>
2019-07-27 09:33:16 +0300

    commit

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class QuestionManager(models.Manager):
+class QuestionManager(models.Manag):
 	def new(self):
 		return self.order_by('-added_at')
 	def popular(self):
