b1f03decce5949e6764a472986b6668bf5d2dddc
%username% <%useremail%>
2019-07-27 09:08:42 +0300

    commit

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -6,19 +6,20 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class QuestionManager(models.Model):
+	def new(self):
+		return self.order_by('-added_at')
+	def popular(self):
+		return self.order_by('-rating')
+
 class Question(models.Model):
+	objects = QuestionManager()
 	title = models.CharField(default="", max_length=1024)
 	text = models.TextField(default="")
 	added_at = models.DateField(null=True)
 	rating = models.IntegerField(default=0)
 	author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 	likes = models.ManyToManyField(User, related_name='likes_set')
-
-class QuestionManager(models.Model):
-	def new(self):
-		return self.order_by('-added_at')
-	def popular(self):
-		return self.order_by('-rating')
 
 class Answer(models.Model):
 	text = models.TextField(default="")
