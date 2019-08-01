c0e9531093961e3014615272cfb742a9c51a595b
%username% <%useremail%>
2019-08-01 14:28:23 +0300

    commit

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import url
-from qa.views import test
+from qa.views import test, new_questions, popular_questions
 
 urlpatterns = [
-	url(r'^$', test, name = 'test'),
+	url(r'^$', new_questions, name='new-questions'),
+	url(r'^popular/', popular_questions, name='popular-questions'),
 	url(r'^login/', test, name = 'test'),
 	url(r'^signup/', test, name = 'test'),
-	url(r'^question/(?P<index>\d+)/', test, name = 'test'),
+	url(r'^question/(?P<index>\d+)/', question_details, name = 'question-details'),
 	url(r'^ask/', test, name = 'test'),
-	url(r'^popular/', test, name = 'test'),
 	url(r'^new/', test, name = 'test'),
 ]
--- ask/qa/views.py
+++ ask/qa/views.py
@@ -4,6 +4,8 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.core.paginator import Paginator
 from qa.models import Question, Answer, QuestionManager
+from django.core.urlresolvers import reverse
+from django.http import Http404, HttpResponse, HttpResponseRedirect
 
 # Create your views here.
 
@@ -12,10 +14,55 @@ from django.http import HttpResponse
 def test(request, *args, **kwargs):
     return HttpResponse('OK')
 
+def new_questions(request):
+	qs = Question.object.new(Question.object.all())
+	try:
+		limit = int(request.GET.get('limit', 10))
+	except:
+		limit = 10
+	try:
+		page = int(request.GET.get('page', 1))
+	except:
+		page = 1
+	paginator = Paginator(qs, limit)
+	paginator.baseurl = reverse('new-questions') + '?page='
+	page = paginator.page(page)
+	return render(request, 'new_questions.html',
+		{'title' : qs.title,
+		'questions' : page.object_list,
+		'paginator' : paginator,
+		'page' : page,}
+	)
 
+def popular_questions(request):
+	qs = Question.object.popular(Question.object.all())
+	try:
+		limit = int(request.GET.get('limit', 10))
+	except:
+		limit = 10
+	try:
+		page = int(request.GET.get('page', 1))
+	except:
+		page = 1
+	paginator = Paginator(qs, limit)
+	paginator.baseurl = reverse('popular-questions') + '?page='
+	page = paginator.page(page)
+	return render(request, 'popular_questions.html',
+		{'title' : qs.title,
+		'questions' : page.object_list,
+		'paginator' : paginator,
+		'page' : page,}
+	)
 
+def question_details(request, id):
+	
+	try:
+		qs = Question.objects.get(id = id)
+	except Question.DoesNotExist:
+		raise Http404
 
-
-
-
-
+	return render(request, 'question_details.html',
+		{'title' : qs.title,
+		'text' : qs.text,
+		'answers' : qs.answer_set.all()}
+	)
