7c095b40df125d6294caf583822e478a0031fe22
VLPC <52045404+VLPC@users.noreply.github.com>
2019-08-04 19:50:46 +0500

    Update urls.py

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
 	url(r'^popular/', popular_questions, name='popular-questions'),
 	url(r'^login/', test, name = 'test'),
 	url(r'^signup/', test, name = 'test'),
-	url(r'^question/(?P<index>\d+)/', question_details, name = 'question-details'),
+	url(r'^question/(?P<pk>\d+)/', question_details, name = 'question-details'),
 	url(r'^ask/', test, name = 'test'),
 	url(r'^new/', test, name = 'test'),
 ]
