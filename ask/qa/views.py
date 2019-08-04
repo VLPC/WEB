9d750f957966894b5a51c2e576b11aea6fb627fd
VLPC <52045404+VLPC@users.noreply.github.com>
2019-08-04 17:52:19 +0500

    Update views.py

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage
 from qa.models import Question, Answer
 from django.core.urlresolvers import reverse
 from django.http import Http404, HttpResponse, HttpResponseRedirect
