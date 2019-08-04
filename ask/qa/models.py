dc3fbc2deab67848136ca311b5b7a4180df5a316
VLPC <52045404+VLPC@users.noreply.github.com>
2019-08-04 18:36:01 +0500

    Update models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -17,7 +17,7 @@ class Question(models.Model):
 	likes = models.ManyToManyField(User, related_name='likes_set')
 	
 	def get_url(self):
-        	return reverse('question', kwargs={'id': self.id})
+        	return reverse('question-details', kwargs={'id': self.id})
 	
 	def __unicode__(self) :
     		return self.title
