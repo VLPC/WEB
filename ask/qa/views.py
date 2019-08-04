f6d6e2be5def93a711d0054a696f14ab44d93a96
VLPC <52045404+VLPC@users.noreply.github.com>
2019-08-04 17:44:23 +0500

    Update views.py

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -14,47 +14,36 @@ from django.http import HttpResponse
 def test(request, *args, **kwargs):
     return HttpResponse('OK')
 
-def paginate(request, qs):
-    try:
-        limit = int(request.GET.get('limit', 10))
-    except ValueError:
-        limit = 10
-
-    try:
-        page = int(request.GET.get('page', 1))
-    except ValueError:
-        raise Http404
-
-    paginator = Paginator(qs, limit)
-
+def new_questions(request):
+    qs = Question.objects.all().order_by('-added_at')
+    page = request.GET.get('page')
+    paginator = Paginator(qs, 10)
     try:
         page = paginator.page(page)
     except EmptyPage:
         page = paginator.page(paginator.num_pages)
-    return page, paginator
-
-def new_questions(request):
-    qs = Question.objects.all().order_by('-added_at')
-    page, paginator = paginate(request, qs)
+    
     paginator.baseurl = reverse('new-questions') + '?page='
 
     return render(request, 'new_questions.html', {
 	'title' : 'New',
         'questions': page.object_list,
-	'paginator': paginator,
-        'page': page,
     })
 
 def popular_questions(request):
 	qs = Question.objects.all().order_by('-rating')
-	page, paginator = paginate(request, qs)
+	page = request.GET.get('page')
+	paginator = Paginator(qs, 10)
+        try:
+        	page = paginator.page(page)
+        except EmptyPage:
+        	page = paginator.page(paginator.num_pages)
 	paginator.baseurl = reverse('popular-questions') + '?page='
 	
 	return render(request, 'popular_questions.html',
 		{'title' : 'Popular',
 		'questions' : page.object_list,
-		'paginator' : paginator,
-		'page' : page,}
+		}
 	)
 
 def question_details(request, num):
