f8f1ea21c4e835d9cb95a65be3378894503eb51f
VLPC <52045404+VLPC@users.noreply.github.com>
2019-08-09 06:53:11 +0300

    Update models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -21,7 +21,7 @@ class Question(models.Model):
 
 class Answer(models.Model):
 	text = models.TextField(default="")
-	added_at = models.DateField(auto_now_add=True)
+	added_at = models.DateTimeField(auto_now_add=True)
 	question = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL)
 	author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 	
