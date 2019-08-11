d7b03b6be9d699f3fb4d0121d96b1da3dc3059fd
VLPC <52045404+VLPC@users.noreply.github.com>
2019-08-11 22:30:06 +0500

    Update forms.py

--- ask/qa/forms.py
+++ ask/qa/forms.py
@@ -5,20 +5,20 @@ class AskForm(forms.Form):
   title = forms.CharField(max_length=120)
   text = forms.CharField(widget=forms.Textarea)
   
+   def clean(self): pass
+  
   def save(self):
     question = Question(**self.cleaned_data)
     question.save()
     return question
   
-  def clean(self): pass
-
 AnswerForm(forms.Form):
   text = forms.CharField(widget=forms.Textarea)
   question = forms.IntegerField(widget=forms.HiddenInput)
+  
+  def clean(self): pass
   
   def save(self):
     answer = Answer(**self.cleaned_data)
     answer.save()
     return answer
-  
-  def clean(self): pass
