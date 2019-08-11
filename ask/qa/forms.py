c6c9b690f0f4cc438d6fac789ce0df40f913dd10
VLPC <52045404+VLPC@users.noreply.github.com>
2019-08-11 14:17:05 +0500

    Update forms.py

--- ask/qa/forms.py
+++ ask/qa/forms.py
@@ -6,7 +6,7 @@ class AskForm(forms.Form):
   text = forms.CharField(widget=forms.Textarea)
   
   def save(self):
-    question = Question.objects.create(self.cleaned_data)
+    question = Question.objects.create(**self.cleaned_data)
     question.save()
     return question
 
@@ -15,6 +15,6 @@ AnswerForm(forms.Form):
   question = forms.IntegerField(widget=forms.HiddenInput)
   
   def save(self):
-    answer = Answer.objects.create(self.cleaned_data)
+    answer = Answer.objects.create(**self.cleaned_data)
     answer.save()
     return answer
