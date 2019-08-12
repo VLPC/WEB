d3666dfe2c09bd4572a3ed47c706405bbe1f0133
VLPC <52045404+VLPC@users.noreply.github.com>
2019-08-12 13:42:22 +0300

    Update urls.py

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.new_questions, name='new-questions'),
 	url(r'^popular/', views.popular_questions, name='popular-questions'),
-	url(r'^login/', views.test, name = 'test'),
-	url(r'^signup/', views.test, name = 'test'),
+	url(r'^login/', views.login, name = 'login'),
+	url(r'^signup/', views.signup, name = 'signup'),
 	url(r'^question/(?P<pk>\d+)/$', views.question_details, name = 'question-details'),
 	url(r'^ask/', views.ask, name = 'ask'),
 	url(r'^new/', views.test, name = 'test'),
