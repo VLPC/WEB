bf9ae0a823a5f168a9ec745b6ddd5041c294bc43
VLPC <52045404+VLPC@users.noreply.github.com>
2019-08-17 22:49:55 +0500

    Update views.py

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -109,7 +109,7 @@ def signup(request):
 			user = form.save()
 			if user is not None:
 				login(request, user)
-				return HttpResponseRedirect('/')
+			return HttpResponseRedirect('/')
 	else:
 		form = SignupForm()
 	return render(request, 'signup.html', {'form' : form})
@@ -124,7 +124,7 @@ def login_view(request):
 			if user is not None:
 				login(request, user)
 				request.session['sessionid'] = user.id
-				return HttpResponseRedirect('/')
+			return HttpResponseRedirect('/')
 	else:
 		form = LoginForm()
 	return render(request, 'login.html', {'form' : form})
