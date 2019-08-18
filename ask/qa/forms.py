7319fa6bbd10f6037baab4976917f5d0fb57de14
VLPC <52045404+VLPC@users.noreply.github.com>
2019-08-18 10:33:05 +0500

    Update forms.py

--- ask/qa/forms.py
+++ ask/qa/forms.py
@@ -100,5 +100,7 @@ class LoginForm(forms.Form):
 		return password
 	
 	def save(self):
-		user = authenticate(**self.cleaned_data)
-		return user
+		user = User(**self.cleaned_data)
+		user.save()
+		auth = authenticate(**self.cleaned_data)
+		return auth
