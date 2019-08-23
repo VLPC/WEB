b15eab4cf64d3569cbb6557c417d8250e26f8dde
VLPC <52045404+VLPC@users.noreply.github.com>
2019-08-23 12:59:46 +0300

    Update views.py

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -123,7 +123,7 @@ def login_view(request):
 			user = authenticate(username=username, password=password)
 			if user is not None:
 				login(request, user)
-			HttpResponse.set_cookie('sessionid', 11)
+			HttpResponse.set_cookie('sessionid')
 			return HttpResponseRedirect('/')
 	else:
 		form = LoginForm()
